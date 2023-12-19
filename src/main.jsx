ea4e72f8a8ef1819578d96877811096ce2ae5fdb
Gianpaoloits <gianpaolo.gabancho.its@gmail.com>
2023-12-19 11:40:20 +0100

    Secondo commit rifattto l'esercizio

--- src/main.jsx
+++ src/main.jsx
@@ -1,38 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import ErrorPage from "./error-page";
-import './index.css'
-import Contact from "./routes/contact";
-import Root, { loader as rootLoader } from "./routes/root";
-
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import "./index.css";
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    loader: rootLoader,
-    children: [
-      {
-        path: "contacts/:contactId",
-        element: <Contact />,
-      },
-    ],
-  },
-  {
-    path: "contacts/:contactId",
-    element: <Contact />,
+    element: <Root/>,
+    errorElement: <ErrorPage/>,
   },
 ]);
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
